d5ce565120987caee2ddddc84b4d02cd2efcbf48
Nicholas Merolle <nmerolle11@hotmail.com>
2020-08-23 21:29:52 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -32,7 +32,7 @@ def full?(board)
 end
 
 def draw?(board)
-    if !won?(board)&&!full?(board)
+    if !won?(board)&&full?(board)
       return true
     elsif full?(board)&&!won?(board)
       return true
