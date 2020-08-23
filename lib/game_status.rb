cd9aeca15f8d0f2cf0ecea51a9202878ed78be71
Nicholas Merolle <nmerolle11@hotmail.com>
2020-08-23 19:24:16 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,4 +8,4 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4
 
 def won?(board)
   WIN_COMBINATIONS.each do |winner|
-    if winner[0]==winner[1]&&winner[1]==winner[2]&&
+    if winner[0]==winner[1]&&winner[1]==winner[2]&&position_taken?(board, index)
